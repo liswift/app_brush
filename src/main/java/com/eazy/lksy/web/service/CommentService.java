9c422cc396d0ff5c5fa54db8f305b74d01f95338
liufengking <liufeng_king@126.com>
2016-08-27 14:52:09 +0800

    删除无关代码

--- src/main/java/com/eazy/lksy/web/service/CommentService.java
+++
@@ -1,9 +0,0 @@
-package com.eazy.lksy.web.service;
-
-import com.eazy.lksy.web.core.dao.CommonService;
-import com.eazy.lksy.web.core.dao.DbEntity;
-
-public interface CommentService extends CommonService {
-
-	public boolean addComment(DbEntity dbEntity);
-}
