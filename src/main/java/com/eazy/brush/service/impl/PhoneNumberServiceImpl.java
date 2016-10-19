b209987ea0214ec70b70325b99c16b87b40d5f20
sunpengshuai <sps457@163.com>
2016-10-19 17:18:17 +0800

    打印调试信息

--- src/main/java/com/eazy/brush/service/impl/PhoneNumberServiceImpl.java
+++ src/main/java/com/eazy/brush/service/impl/PhoneNumberServiceImpl.java
@@ -4,10 +4,10 @@ import com.eazy.brush.core.utils.HttpUtil;
 import com.eazy.brush.core.utils.NiuMaUtil;
 import com.eazy.brush.service.PhoneNumberService;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 /**
  * Created by yuekuapp on 16-10-18.
@@ -16,11 +16,11 @@ import javax.annotation.PostConstruct;
 @Slf4j
 public class PhoneNumberServiceImpl implements PhoneNumberService {
 
-//    @Value("${niuma.uid}")
-    private String username="xuguang91915";
+    //    @Value("${niuma.uid}")
+    private String username = "xuguang91915";
 
-//    @Value("${niuma.pwd}")
-    private String password="xuguang";
+    //    @Value("${niuma.pwd}")
+    private String password = "xuguang";
 
     private String token;
 
@@ -28,51 +28,53 @@ public class PhoneNumberServiceImpl implements PhoneNumberService {
     @PostConstruct
     @Override
     public void loginIn() {
-        if(StringUtils.isEmpty(token)){
-            log.info("begin login niuma!");
-            String result = HttpUtil.get(NiuMaUtil.getLogin(username,password));
-            log.info("login result:"+result);
-            if(result.contains(username)){
-                token=result.split("\\|")[1];
-            }else{
-                log.info("niu ma login error!"+result);
-            }
+        log.info("begin login niuma!");
+        String result = HttpUtil.get(NiuMaUtil.getLogin(username, password));
+        log.info("login result:" + result);
+        if (result.contains(username)) {
+            token = result.split("\\|")[1];
+        } else {
+            log.info("niu ma login error!" + result);
         }
     }
 
     @Override
     public String getMobileNum(String pid, int size) {
         loginIn();
-        releaseMobileNum("");//获取新的号码之前,首先清空旧的号码
-        String result="";
-        while(!result.contains(token)){
-            result=HttpUtil.get(NiuMaUtil.getMobileNum(pid,username,token));
-            log.info("niuma getmobile num :"+result);
+        String result = "";
+        while (!result.contains(token)) {
+            result = HttpUtil.get(NiuMaUtil.getMobileNum(pid, username, token));
+            log.info("niuma getmobile num :" + result);
         }
         return result.split("\\|")[0];
     }
 
     @Override
     public void releaseMobileNum(String mobile) {
-        loginIn();
-        HttpUtil.get(NiuMaUtil.releaseMobileNum(username,token,mobile));
+        HttpUtil.get(NiuMaUtil.releaseMobileNum(username, token, mobile));
     }
 
     @Override
     public String getMobileCodeAndRelease(String pid, String mobile) {
         loginIn();
-        String result= HttpUtil.get(NiuMaUtil.getVcodeAndReleaseMobile(username,token,pid,mobile));
-        if(result.contains(mobile)){
+        String result = HttpUtil.get(NiuMaUtil.getVcodeAndReleaseMobile(username, token, pid, mobile));
+        if (result.contains(mobile)) {
             return result.split("|")[1];
         }
-        log.info("get mobile code error :"+result);
+        log.info("get mobile code error :" + result);
         return "";
     }
 
     @Override
     public void addIgnoreList(String mobiles, String pid) {
         loginIn();
-        HttpUtil.get(NiuMaUtil.addIgnoreList(username,token,mobiles,pid));
+        HttpUtil.get(NiuMaUtil.addIgnoreList(username, token, mobiles, pid));
+    }
+
+
+    @PreDestroy
+    private void desotry(){
+        releaseMobileNum("");
     }
 
 
